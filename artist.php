b89629667a2a633092419ade34f0961b7e5e0468
yasuhitotanaka <sitofourth02@gmail.com>
2018-09-14 09:27:57 +0700

    add artist page

---
+++ artist.php
@@ -0,0 +1,89 @@
+<?php
+  include("includes/includedFiles.php");
+
+  if (isset($_GET['id'])) {
+    $artistID = $_GET['id'];
+  } else {
+    header("Location: indx.php");
+  }
+
+  $artist = new Artist($connection, $artistID);
+?>
+
+<div class="entityInfo borderBottom">
+  <div class="centerSection">
+    <div class="artistInfo centerizeText">
+      <h1 class="artistName"><?php echo $artist->getName() ?></h1>
+
+      <div class="headerButtons">
+        <button class="button green" onclick="playFirstSong()">PLAY</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="trackListContainer borderBottom">
+  <h2 class="centerizeText">SONGS</h2>
+  <ul class="trackList">
+    <?php
+      $songIDArray = $artist->getSongIDs();
+      $i = 1;
+      foreach($songIDArray as $songID) {
+
+        if ($i > 5) {
+          break;
+        }
+
+        $albumSong = new Song($connection, $songID);
+        $albumArtist = $albumSong->getArtist();
+
+        echo "<li class='tracklistRow'>
+                <div class='trackCount'>
+                  <img class='play' src='assets/images/icons/play_white.png' onclick='setTrack(\"" .  $albumSong->getID()  . "\", temporaryPlayList, true)'>
+                  <span class='trackNumber'>$i</span>
+                </div>
+
+                <div class='trackInfo'>
+                  <span class='trackName'>" . $albumSong->getTitle() . "</span>
+                  <span class='artistName'>" . $albumArtist->getName() . "</span>
+                </div>
+
+                <div class='trackOptions'>
+                  <img class='optionButton' src='assets/images/icons/more.png'>
+                </div>
+
+                <div class='trackDuration'>
+                  <span class='duration'>" . $albumSong->getDuration() . "</span>
+                </div>
+              </li>";
+
+        $i++;
+      }
+    ?>
+
+    <script>
+      var tempSongIDs = '<?php echo json_encode($songIDArray) ?>';
+      temporaryPlayList = JSON.parse(tempSongIDs);
+    </script>
+
+  </ul>
+</div>
+
+<div class="gridViewContainer">
+  <h2 class="centerizeText">ALBUMS</h2>
+  <?php
+    $albumQuery = mysqli_query($connection, "SELECT * FROM albums WHERE artist='$artistID'");
+
+    while($row = mysqli_fetch_array($albumQuery)) {
+
+      echo "<div class='gridViewItem'>
+              <span role='link' tabindex='0' onclick='openPage(\"album.php?id=" . $row['id'] . "\")'>
+                <img src='" . $row['artworkPath'] .  "'>
+                <div class='gridViewInfo'>"
+                . $row['title'] .
+                "</div>
+              </span>
+            </div>";
+    }
+   ?>
+</div>
--- includes/classes/Artist.php
+++ includes/classes/Artist.php
@@ -8,10 +8,20 @@
       $this->connection = $connection;
       $this->id = $id;
     }
+
     public function getName() {
       $artistQuery = mysqli_query($this->connection, "SELECT name FROM artists WHERE id='$this->id'");
       $artist = mysqli_fetch_array($artistQuery);
       return $artist['name'];
+    }
+
+    public function getSongIDs() {
+      $songs = array();
+      $songsQuery = mysqli_query($this->connection, "SELECT id FROM songs WHERE artist='$this->id' ORDER BY albumOrder ASC");
+      while($row = mysqli_fetch_array($songsQuery)) {
+        array_push($songs, $row['id']);
+      }
+      return $songs;
     }
   }
 ?>
